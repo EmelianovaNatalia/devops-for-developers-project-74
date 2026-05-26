40846c5a65e9d1f44544924a797459cc06a488a8
NataliaEmelianova <emelianovawork@gmail.com>
2026-05-26 13:18:51 +0300

    Fix compatibility with Node 14 and sequelize dialect

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,13 +1,34 @@
-const config = {
-  test: {
-    username: 'postgres',
-    password: 'postgres',
-    database: 'test_db',
-    host: 'db',
-    port: 5432,
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'postgres',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
+    dialectModule: require('pg'),
+    logging: false
+  },
+  test: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'test_db',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
+    dialectModule: require('pg'),
+    logging: false
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectModule: require('pg'),
     logging: false
   }
 };
-
-module.exports = config;
