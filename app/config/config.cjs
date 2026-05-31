18c6e0c7130f9d8ad33a24699dd8b615e954af31
NataliaEmelianova <emelianovawork@gmail.com>
2026-05-31 15:56:44 +0300

    fix: restore original package.json and config.cjs

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,17 +1,19 @@
+require('dotenv').config();
 module.exports = {
   development: {
     dialect: 'sqlite',
-    storage: '.sqlite',
-    logging: false
+    storage: './database.sqlite',
+  },
+  production: {
+    dialect: 'postgres',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
   },
   test: {
     dialect: 'sqlite',
-    storage: ':memory:',
-    logging: false
+    storage: './database.test.sqlite',
   },
-  production: {
-    dialect: 'sqlite',
-    storage: '.sqlite',
-    logging: false
-  }
 };
