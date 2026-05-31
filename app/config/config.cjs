af094c4bf9b1f2ed53d1fd9e41dffac80692923d
NataliaEmelianova <emelianovawork@gmail.com>
2026-05-31 15:48:40 +0300

    fix: add development and production dialects to config.cjs

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -1,7 +1,17 @@
 module.exports = {
+  development: {
+    dialect: 'sqlite',
+    storage: '.sqlite',
+    logging: false
+  },
   test: {
     dialect: 'sqlite',
     storage: ':memory:',
+    logging: false
+  },
+  production: {
+    dialect: 'sqlite',
+    storage: '.sqlite',
     logging: false
   }
 };
